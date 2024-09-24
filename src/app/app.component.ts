f66397dc2fc51ee2ebed8cad0e259f5a94b68d49
Simon Ziervogel <simon-ziervogel@web.de>
2024-09-24 10:08:18 +0200

    first commit: insertion of logo, setting up base structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { HomeComponent } from "./home/home.component";
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
